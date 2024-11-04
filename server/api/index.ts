6e0532c47272b13cca8ec1dc43721c4eea1a5292
Neta <Neta-Rose@users.noreply.github.com>
2024-11-04 16:46:26 +0200

    fix(index.ts, data-source.ts): Fix dotenv config location

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,56 +1,3 @@
-// import { Request, Response } from "express";
-// import express from "express";
-// import { myDataSource } from "../src/connection/data-source";
-// import { errorHandler } from "../src/middleware/errorHandler.middleware";
-// import logger from "../src/logger/logger";
-// import restaurantRouter from "../src/routes/restaurant.router";
-// import orderRouter from "../src/routes/order.router";
-// import deliveryManRouter from "../src/routes/deliveryMan.router";
-// import customerRouter from "../src/routes/customer.router";
-// import menuRouter from "../src/routes/menu.router";
-// import { loggerMiddleware } from "../src/middleware/logger.middleware";
-// import cors from 'cors';
-// import dotenv from "dotenv";
-
-// const app = express();
-
-// export default async(req:Request, res:Response) => {
-//     startServer();
-//     app(req,res)
-// }
-
-
-// const startServer = async ()=> {
-//     dotenv.config();
-//     if (!myDataSource.isInitialized) {
-//         try {
-//             await myDataSource.initialize();
-//             console.log("Database connection established.");
-//         } catch (error) {
-//             console.error("Error during Data Source initialization", error);
-//             throw error; 
-//         }
-//     } else {
-//         console.log("Database connection already established.");
-//     }
-   
-//      app.get('/', (req, res) => {res.send('Hello World!')});
-
-//     app.use(express.json());
-//     app.use(cors());
-
-//     app.use(loggerMiddleware);
-//     app.use("/restaurants", restaurantRouter);
-//     app.use("/order", orderRouter);
-//     app.use("/delivery-mans", deliveryManRouter);
-//     app.use("/customers", customerRouter);
-//     app.use("/menus", menuRouter)
-
-//     app.use(errorHandler);
-// }
-
-// api/index.ts
-
 import express, { Request, Response } from 'express';
 import { myDataSource } from '../src/connection/data-source';
 import { errorHandler } from '../src/middleware/errorHandler.middleware';
@@ -62,9 +9,6 @@ import customerRouter from '../src/routes/customer.router';
 import menuRouter from '../src/routes/menu.router';
 import { loggerMiddleware } from '../src/middleware/logger.middleware';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 
@@ -78,6 +22,12 @@ app.use('/order', orderRouter);
 app.use('/delivery-mans', deliveryManRouter);
 app.use('/customers', customerRouter);
 app.use('/menus', menuRouter);
+
+app.listen(3030, () => { 
+    logger.info('Server started on port: 3030') 
+});
+
+
 
 app.use(errorHandler);
 
@@ -95,6 +45,10 @@ const initializeDb = async () => {
     }
   }
 };
+
+if(process.env.NODE_ENV === 'development') {  
+    initializeDb(); 
+}
 
 export default async (req: Request, res: Response) => {
   try {
--- server/src/connection/data-source.ts
+++ server/src/connection/data-source.ts
@@ -1,13 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const myDataSource: DataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: String(process.env.DB_PASSWORD),
   database: "postgres",
   entities: ["./**/entities/typeORM/*.ts"],
   logging: false,
