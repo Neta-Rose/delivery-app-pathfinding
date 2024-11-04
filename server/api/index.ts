d79d8f9208bad1bf6d1c85c3d2425bb19d19d37e
Neta <Neta-Rose@users.noreply.github.com>
2024-11-04 08:56:31 +0200

    fix(vite.config.ts): Fix vite config file for deployment

---
+++ server/api/index.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import express from "express";
+import { myDataSource } from "../src/connection/data-source";
+import { errorHandler } from "../src/middleware/errorHandler.middleware";
+import logger from "../src/logger/logger";
+import restaurantRouter from "../src/routes/restaurant.router";
+import orderRouter from "../src/routes/order.router";
+import deliveryManRouter from "../src/routes/deliveryMan.router";
+import customerRouter from "../src/routes/customer.router";
+import menuRouter from "../src/routes/menu.router";
+import { loggerMiddleware } from "../src/middleware/logger.middleware";
+import cors from 'cors';
+
+const app = express();
+
+export default async(req:Request, res:Response) => {
+    startServer();
+    app(req,res)
+}
+
+
+const startServer = ()=> {
+    myDataSource
+    .initialize()
+    logger.info(`DB initialized`);
+    const app: express.Express = express();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.use(loggerMiddleware);
+    app.use("/restaurants", restaurantRouter);
+    app.use("/order", orderRouter);
+    app.use("/delivery-mans", deliveryManRouter);
+    app.use("/customers", customerRouter);
+    app.use("/menus", menuRouter)
+
+    app.use(errorHandler);
+}
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,39 +1,40 @@
-import express from "express";
-import { myDataSource } from "./connection/data-source";
-import { errorHandler } from "./middleware/errorHandler.middleware";
-import logger from "./logger/logger";
-import restaurantRouter from "./routes/restaurant.router";
-import orderRouter from "./routes/order.router";
-import deliveryManRouter from "./routes/deliveryMan.router";
-import customerRouter from "./routes/customer.router";
-import menuRouter from "./routes/menu.router";
-import { loggerMiddleware } from "./middleware/logger.middleware";
-import cors from 'cors';
+  import express from "express";
+  import { myDataSource } from "./connection/data-source";
+  import { errorHandler } from "./middleware/errorHandler.middleware";
+  import logger from "./logger/logger";
+  import restaurantRouter from "./routes/restaurant.router";
+  import orderRouter from "./routes/order.router";
+  import deliveryManRouter from "./routes/deliveryMan.router";
+  import customerRouter from "./routes/customer.router";
+  import menuRouter from "./routes/menu.router";
+  import { loggerMiddleware } from "./middleware/logger.middleware";
+  import cors from 'cors';
 
-myDataSource
-  .initialize()
-  .then(async () => {
-    logger.info(`DB initialized`);
-    const app: express.Express = express();
+  
+  myDataSource
+    .initialize()
+    .then(async () => {
+      logger.info(`DB initialized`);
+      const app: express.Express = express();
 
-    app.use(express.json());
-    app.use(cors());
+      app.use(express.json());
+      app.use(cors());
 
-    app.use(loggerMiddleware);
-    app.use("/restaurants", restaurantRouter);
-    app.use("/order", orderRouter);
-    app.use("/delivery-mans", deliveryManRouter);
-    app.use("/customers", customerRouter);
-    app.use("/menus", menuRouter)
+      app.use(loggerMiddleware);
+      app.use("/restaurants", restaurantRouter);
+      app.use("/order", orderRouter);
+      app.use("/delivery-mans", deliveryManRouter);
+      app.use("/customers", customerRouter);
+      app.use("/menus", menuRouter)
 
-    app.use(errorHandler);
+      app.use(errorHandler);
 
-    const port = 3030;
-    app.listen(port, () => {
-      logger.info(`Server started on port: ${port}`);
+      const port = 3030;
+      app.listen(port, () => {
+        logger.info(`Server started on port: ${port}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      logger.error(error)
     });
-  })
-  .catch((error) => {
-    console.log(error);
-    logger.error(error)
-  });
