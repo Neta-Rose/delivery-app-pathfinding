4e084e95874dcf8d3bde733f7a75ab204744a19b
Neta <Neta-Rose@users.noreply.github.com>
2024-11-07 17:48:03 +0200

    fix(.env, axiosInstance.ts, index.ts): Fix db initialization

--- server/api/index.ts
+++ server/api/index.ts
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(loggerMiddleware);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Aura server!');
+    res.send('Hello World');
 });
 
 app.use('/restaurants', restaurantRouter);
