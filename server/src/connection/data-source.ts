5bdc6d5628bcefdced556efafb5a2eafb225b006
Neta <Neta-Rose@users.noreply.github.com>
2024-11-07 18:13:41 +0200

    fix(.env, axiosInstance.ts, index.ts): Fix db initialization

--- server/src/connection/data-source.ts
+++ server/src/connection/data-source.ts
@@ -1,49 +1,23 @@
-// import "reflect-metadata";
-// import { DataSource } from "typeorm";
-// import { SnakeNamingStrategy } from "typeorm-naming-strategies";
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export const myDataSource: DataSource = new DataSource({
-//   type: "postgres",
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
-//   username: process.env.DB_USER,
-//   password: String(process.env.DB_PASSWORD),
-//   database: "postgres",
-//   entities: ["./**/entities/typeORM/*.ts"],
-//   logging: false,
-//   synchronize: false,
-//   namingStrategy: new SnakeNamingStrategy(),
-//   schema: "delivery", 
-// });
-
-
-// data-source.ts
-
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export const myDataSource: DataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: String(process.env.DB_PASSWORD),
-  database: process.env.DB_NAME,
-  entities: isProduction
-    ? ['dist/src/entities/typeORM/*.js']
-    : ['src/entities/typeORM/*.ts'],
+  database: "postgres",
+  entities: process.env.NODE_ENV === 'production'
+  ? ['./**/**/**/dist/src/entities/typeORM/*.js']
+  : ['./**/**/src/entities/typeORM/*.ts'], //"./**/entities/typeORM/*.ts"
   logging: false,
   synchronize: false,
   namingStrategy: new SnakeNamingStrategy(),
-  schema: 'delivery',
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  schema: "delivery", 
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
