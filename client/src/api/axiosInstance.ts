c5c0455593101c7f1b2f0802e1d58512a3247561
Neta <Neta-Rose@users.noreply.github.com>
2024-11-13 11:46:32 +0200

    fix(axiosInstance.ts): Fix baseURL to prod deploy

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://delivery-app-pathfinding-backend.vercel.app/",
+  baseURL: "https://delivery-app-pathfinding-neta-rose-server.vercel.app/",
 });
